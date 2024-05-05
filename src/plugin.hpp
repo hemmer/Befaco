61abb442cff2e862b3a01bf7f864df86146b431e
hemmer <915048+hemmer@users.noreply.github.com>
2024-05-05 20:52:38 +0100

    Remove ladder tests for now

--- src/plugin.hpp
+++ src/plugin.hpp
@@ -34,7 +34,7 @@ extern Model* modelMidiThing;
 extern Model* modelVoltio;
 extern Model* modelOctaves;
 extern Model* modelPonyVCF;
-extern Model* modelLadderVCF;
+// extern Model* modelLadderVCF;
 
 struct Knurlie : SvgScrew {
 	Knurlie() {
